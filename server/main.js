1fac7cfc98dfbcbb46158fd2653571af0d6f5725
unknown <ezekuiel100@gmail.com>
2024-07-23 15:36:53 -0300

    Server

---
+++ server/main.js
@@ -0,0 +1,28 @@
+import express from "express";
+const app = express();
+import fs from "fs";
+
+import cors from "cors";
+
+app.use(cors({ origin: "http://localhost:5173" }));
+
+app.use(express.json({ limit: "100mb" }));
+
+app.post("/upload", (req, res) => {
+  const { file, fileName } = req.body;
+
+  const filePath = `./videos/${fileName}`;
+
+  fs.writeFile(filePath, file, (err) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: "Erro ao salvar vídeo" });
+    }
+
+    res.status(400).json({ message: "Vídeo salvo com sucesso.", filePath });
+  });
+});
+
+app.get("/", () => {});
+
+app.listen("3000", () => console.log("Server is running on port 3000"));
--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,51 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useRef } from "react";
 
 function App() {
   const [video, setvideo] = useState(null);
   const ref = useRef();
 
-  function file(e) {
+  function handleFile(e) {
+    const file = ref.current.files[0];
+    const videoUrl = URL.createObjectURL(file);
+    setvideo(videoUrl);
+  }
+
+  function sendVideo() {
+    const file = ref.current.files[0];
+    const fileName = file.name;
+
     const reader = new FileReader();
 
-    reader.onload = (e) => {
-      if (!e.target.result.startsWith("data:video")) return;
-      setvideo(e.target.result);
+    reader.onloadend = () => {
+      const base64File = reader.result.split(",")[1];
+
+      fetch("http://localhost:3000/upload", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          file: base64File,
+          fileName: fileName,
+        }),
+      })
+        .then((res) => res.json())
+        .then((data) => console.log("Sucesso:", data))
+        .catch((error) => console.log("Erro:", error));
     };
-    reader.readAsDataURL(ref.current.files[0]);
+
+    reader.readAsDataURL(file);
   }
 
   return (
     <>
-      <h1>Video</h1>
-      <input type="file" ref={ref} onChange={file} />
-      <video src={video} controls style={{ height: 400 }} />
+      <h1 className="text-xl">Send Video</h1>
+      <input type="file" accept="video/mp4" ref={ref} onChange={handleFile} />
+      <video src={video} controls className={`${video ? "h-96" : "hidden"} `} />
+      <button onClick={sendVideo} className="bg-blue-300 p-2 m-2">
+        Enviar video
+      </button>
     </>
   );
 }
