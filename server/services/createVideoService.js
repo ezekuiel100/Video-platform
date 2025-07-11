37f59b72ba8c343afd8dde805b20b8bda8da3565
unknown <ezekuiel100@gmail.com>
2025-07-10 22:17:19 -0300

    refactored

--- server/services/createVideoService.js
+++ server/services/createVideoService.js
@@ -1,20 +1,39 @@
 import { prisma } from "../lib/prisma.js";
+import createPresignedUrl from "./createPresignedUrl.js"
+import generateUniqueFileName from "./generateUniqueFileName.js";
 
 export default async function createVideoService(
-  title,
-  thumbnail,
-  videoUploadResponse,
-  channel
+  title, fileName, fileType, thumbName, thumbType, channel
 ) {
-  const newVideo = await prisma.video.create({
-    data: {
-      title,
-      content: "",
-      thumbnail: null,
-      url: videoUploadResponse,
-      channelId: channel,
-    },
-  });
 
-  return newVideo;
+  try {
+    let uniqueThumbName
+    const uniqueFileName = generateUniqueFileName(fileName)
+
+    let thumbUrl
+    const videoUrl = await createPresignedUrl(uniqueFileName, fileType);
+    console.log("URL Presigned gerada:", videoUrl)
+
+    if (thumbName) {
+      uniqueThumbName = generateUniqueFileName(thumbName)
+      thumbUrl = await createPresignedUrl(uniqueThumbName, thumbType);
+    }
+
+    const baseUrl = `https://aa6766c4dc5f1a33005c52f5f1f5c306.r2.cloudflarestorage.com/video-platform`
+
+    await prisma.video.create({
+      data: {
+        title,
+        content: "",
+        thumbnail: thumbName ? `${baseUrl}/thumbnail/${uniqueThumbName}` : "",
+        url: `${baseUrl}/videos/${uniqueFileName}`,
+        channelId: channel,
+      },
+    });
+
+
+    return { videoUrl, thumbUrl };
+  } catch (error) {
+    console.log("Erro ao criar video", error.message)
+  }
 }
