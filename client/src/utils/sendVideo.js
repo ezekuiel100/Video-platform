4ac80e797065b1055297f7acb06e80eff6a7c1d2
unknown <ezekuiel100@gmail.com>
2025-07-10 22:12:31 -0300

    refactored

--- client/src/utils/sendVideo.js
+++ client/src/utils/sendVideo.js
@@ -6,49 +6,39 @@ export async function sendVideo(title, file, thumbnail, user) {
     return;
   }
 
-  const fileName = file.name;
-  const thumbName = thumbnail?.name;
-
-  function fileToBase64(file) {
-    return new Promise((resolve, reject) => {
-      const reader = new FileReader();
-      reader.readAsDataURL(file);
-
-      reader.onloadend = () => {
-        resolve(reader.result.split(",")[1]);
-      };
-
-      reader.onerror = (error) => {
-        reject(error);
-      };
-    });
-  }
-
-  const base64File = await fileToBase64(file);
-
-  let thumb = null;
-
-  if (thumbnail) {
-    thumb = await fileToBase64(thumbnail);
-  }
-
-  axios
-    .post(
+  try {
+    const res = await axios.post(
       "http://localhost:3000/upload",
       {
-        file: base64File,
-        fileName: fileName,
-        thumbName,
         title,
-        thumbnail: thumb,
+        fileName: file.name,
+        fileType: file.type,
+        fileSize: file.size,
+        thumbName: thumbnail?.name,
+        thumbType: thumbnail?.type,
         channel: user.channel.id,
       },
       { withCredentials: true }
-    )
-    .then((data) => {
-      if (data.status == 200) {
-        window.location.reload();
-      }
-    })
-    .catch((error) => console.log("Erro:", error));
+    );
+
+    const urlPresigned = res.data.videoUrl;
+    console.log(file.type)
+
+    // Aguarde a resposta do fetch
+    const uploadResponse = await fetch(urlPresigned, {
+      method: "PUT",
+      headers: {
+        "Content-Type": file.type, // Tipo do arquivo
+      },
+      body: file, // O arquivo que você deseja enviar
+    });
+
+    if (!uploadResponse.ok) {
+      throw new Error("Erro ao enviar o vídeo");
+    }
+
+    console.log("Upload realizado com sucesso!");
+  } catch (error) {
+    console.log("Erro:", error);
+  }
 }
