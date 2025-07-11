398ed292c3ac25c6f9f5cec6f922d199635c8d02
unknown <ezekuiel100@gmail.com>
2025-07-10 21:29:36 -0300

    create Presigned Url

---
+++ server/services/createPresignedUrl.js
@@ -0,0 +1,26 @@
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import {
+    getSignedUrl,
+} from "@aws-sdk/s3-request-presigner";
+
+export default async function createPresignedUrl(Key, ContentType) {
+    if (
+        !process.env.CLOUDFLARE_ENDPOINT ||
+        !process.env.ACCESS_KEY ||
+        !process.env.SECRET_KEY
+    ) {
+        throw new Error("Variáveis de ambiente não definidas.");
+    }
+
+    const client = new S3Client({
+        region: "auto", endpoint: process.env.CLOUDFLARE_ENDPOINT,
+        credentials: {
+            accessKeyId: process.env.ACCESS_KEY,
+            secretAccessKey: process.env.SECRET_KEY,
+        },
+    });
+
+    const command = new PutObjectCommand({ Bucket: "video-platform", Key, ContentType });
+
+    return getSignedUrl(client, command, { expiresIn: 3600 });
+}
