883cf07009afb3f122cd985c4281861cd57b85df
unknown <ezekuiel100@gmail.com>
2025-07-12 10:28:59 -0300

    refactor: Enhance create video service logic

--- server/services/createVideoService.js
+++ server/services/createVideoService.js
@@ -3,16 +3,15 @@ import createPresignedUrl from "./createPresignedUrl.js"
 import generateUniqueFileName from "./generateUniqueFileName.js";
 
 export default async function createVideoService(
-  title, fileName, fileType, thumbName, thumbType, channel
+  title, fileName, fileType, thumbName, thumbType, userId
 ) {
 
   try {
     let uniqueThumbName
     const uniqueFileName = generateUniqueFileName(fileName)
 
-    let thumbUrl
+    let thumbUrl = null
     const videoUrl = await createPresignedUrl(uniqueFileName, fileType);
-    // console.log("URL Presigned gerada:", videoUrl)
 
     if (thumbName) {
       uniqueThumbName = generateUniqueFileName(thumbName)
@@ -21,13 +20,25 @@ export default async function createVideoService(
 
     const baseUrl = `https://pub-bb2774a25cae4bb28b9032cb5d338770.r2.dev`
 
+    const user = await prisma.user.findUnique({
+      where: { id: userId },
+      select: { channel: { select: { id: true } } }
+    })
+
+    if (!user) {
+      throw new Error(`Usuário com ID ${userId} não encontrado.`);
+    }
+    if (!user.channel) {
+      throw new Error(`Usuário com ID ${userId} não possui um canal associado.`);
+    }
+
     await prisma.video.create({
       data: {
         title,
         content: "",
         thumbnail: thumbName ? `${baseUrl}/thumbnail/${uniqueThumbName}` : "",
         url: `${baseUrl}/video/${uniqueFileName}`,
-        channelId: channel,
+        channelId: user.channel.id,
       },
     });
 
@@ -35,5 +46,6 @@ export default async function createVideoService(
     return { videoUrl, thumbUrl };
   } catch (error) {
     console.log("Erro ao criar video", error.message)
+    return { error: error.message }
   }
 }
