3ca4042af548b052a635d29c59bdf51ab9062b56
unknown <ezekuiel100@gmail.com>
2025-07-12 19:25:37 -0300

    change file name

---
+++ client/src/page/UploadVideoPage.jsx
@@ -0,0 +1,37 @@
+import { useRef, useState } from "react";
+import Nav from "../components/Nav";
+import VideoUpload from "../components/VideoUpload";
+import SelectThumbnail from "../components/SelectThumbnail";
+
+function UploadVideoPage() {
+  const [title, setTitle] = useState("");
+  const refImg = useRef(null);
+
+  return (
+    <>
+      <Nav />
+      <div className='text-black h-screen flex justify-center items-center'>
+        <div className='grid grid-cols-2 w-[50rem] h-[30rem] bg-blue-400 rounded-3xl'>
+          <div className='flex flex-col items-center gap-20 px-8 py-12 '>
+            <div className='w-full'>
+              <label htmlFor='title'>Title:</label>
+              <input
+                type='text'
+                name='title'
+                required
+                value={title}
+                className='w-full p-2 rounded-lg outline-none'
+                onChange={(e) => setTitle(e.target.value)}
+              />
+            </div>
+            <SelectThumbnail ref={refImg} />
+          </div>
+
+          <VideoUpload refImg={refImg} title={title} />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default UploadVideoPage;
