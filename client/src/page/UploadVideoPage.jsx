7148aa548cdd25a8a0846b150d0fabc072a5f1d3
unknown <ezekuiel100@gmail.com>
2025-07-17 18:33:55 -0300

    feat: redesign video upload section

--- client/src/page/UploadVideoPage.jsx
+++ client/src/page/UploadVideoPage.jsx
@@ -1,36 +1,26 @@
-import { useRef, useState } from "react";
 import Nav from "../components/Nav";
-import VideoUpload from "../components/VideoUpload";
-import SelectThumbnail from "../components/SelectThumbnail";
 
 function UploadVideoPage() {
-  const [title, setTitle] = useState("");
-  const refImg = useRef(null);
-
   return (
-    <>
+    <div className="min-h-screen flex flex-col">
       <Nav />
-      <div className='text-black h-screen flex justify-center items-center'>
-        <div className='grid grid-cols-2 w-[50rem] h-[30rem] bg-blue-400 rounded-3xl'>
-          <div className='flex flex-col items-center gap-20 px-8 py-12 '>
-            <div className='w-full'>
-              <label htmlFor='title'>Title:</label>
-              <input
-                type='text'
-                name='title'
-                required
-                value={title}
-                className='w-full p-2 rounded-lg outline-none'
-                onChange={(e) => setTitle(e.target.value)}
-              />
-            </div>
-            <SelectThumbnail ref={refImg} />
-          </div>
+      <div className="flex-1 text-black flex justify-center items-center">
+        <div className="bg-white w-[28rem] h-[20rem] rounded-xl py-10 px-12 flex flex-col ">
+          <h1 className="text-center text-2xl mb-3 text-blue-800 font-bold">Selecione o Vídeo</h1>
 
-          <VideoUpload refImg={refImg} title={title} />
+          <label htmlFor="upload" className="flex-1 flex items-center justify-center cursor-pointer">
+            <div className="w-full h-full border-2 border-blue-500 border-dashed flex flex-col items-center justify-center rounded-xl">
+              <svg className="w-12 h-12 text-blue-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5" />
+              </svg>
+              <h2 className="text-blue-800 font-semibold text-lg">Clique ou arraste o vídeo aqui</h2>
+              <p className="text-gray-500">Até 100MB</p>
+              <input type="file" accept="video/*" className="hidden" id="upload" />
+            </div>
+          </label>
         </div>
       </div>
-    </>
+    </div >
   );
 }
 
